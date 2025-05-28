c63b57d5a73c118aea9152bff6487cebb4e55d43
Eman <emanabukhater2001@gmail.com>
2025-05-28 16:49:12 +0300

    Set up ToastContainer in Layout

--- src/app/components/AuthProvider.js
+++ src/app/components/AuthProvider.js
@@ -1,6 +1,12 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function AuthProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+    <>
+      return <SessionProvider>{children}</SessionProvider>;
+      <ToastContainer position="top-right" autoClose={2000} />
+
+
+    </>
 }
