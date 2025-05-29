ff6b84926d807a1823a5a17792fc282b664ffd76
Eman <emanabukhater2001@gmail.com>
2025-05-29 18:15:46 +0300

    Create a QueryClient Provider

--- src/app/components/AuthProvider.js
+++ src/app/components/AuthProvider.js
@@ -1,15 +1,23 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 export default function AuthProvider({ children }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
+      <QueryClientProvider client={queryClient}>
 
-      <SessionProvider>
-        <ToastContainer position="top-right" autoClose={2000} />
-        {children}
-      </SessionProvider>
+        <SessionProvider>
+          <ToastContainer position="top-right" autoClose={2000} />
+          {children}
+        </SessionProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 
